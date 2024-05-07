798aa89373faf30da8ef7bd91bd88b249656fa98
esztermirjam <esztermirjam@gmail.com>
2024-05-07 20:55:20 +0200

    Final_application

--- recipes/app/src/main/java/hu/bme/aut/android/recipes_app/feature/recipes/RecipesDataHolder.kt
+++
@@ -1,8 +0,0 @@
-package hu.bme.aut.android.recipes_app.feature.recipes
-
-import hu.bme.aut.android.recipes_app.models.RecipeDetails
-import hu.bme.aut.android.recipes_app.models.RecipeDetailsList
-
-interface RecipesDataHolder {
-    fun getRecipeMain(): RecipeDetailsList?
-}
